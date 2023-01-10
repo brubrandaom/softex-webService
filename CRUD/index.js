b15c0b11ce890925afc48542cb4a257a3456625f
brubrandaom <85810656+brubrandaom@users.noreply.github.com>
2023-01-10 16:18:43 -0300

    Add files via upload

---
+++ CRUD/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/livros', (req, res) => {
+    const livros = [
+        {
+            "id": 1,
+            "nome": "Orgulho e Preconceito",
+            "autor": "Jane Austen",
+            "ISBN": "000-0-00-000000-0"
+        },
+        {
+            "id": 2,
+            "nome": "O retrato de Dorian Gray",
+            "autor": "Oscar Wild",
+            "IBSN": "111-1-11-111111-1"
+        },
+        {
+            "id": 3,
+            "nome": "Carta ao pai",
+            "autor": "Kafka",
+            "IBSN": "222-2-22-222222-2"
+        }
+    ];
+    res.json(livros);
+});
+
+app.get('/livros/:id', (req, res) => {
+    const idLivro = req.params;
+    res.json(idLivro);
+});
+
+app.post('/livros', (req, res) => {
+    res.json(req.body);
+});
+
+app.put('/livros/:id', (req, res) => {
+    const idLivro = req.params;
+    res.json(req.body);
+});
+
+app.delete('/livros/:id', (req, res) => {
+    const idLivro = req.params;
+    res.json(idLivro);
+});
+
+app.listen(8080, () => {
+    console.log('Servidor Funcionando!');
+});
