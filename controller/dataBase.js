042cc6f2cee705a51a113e376a9d13f5ef0c30d7
brubrandaom <85810656+brubrandaom@users.noreply.github.com>
2023-01-16 21:29:50 -0300

    Add files via upload

---
+++ controller/dataBase.js
@@ -0,0 +1,19 @@
+var livros = [
+    {
+        "id": 1,
+        "nome": "Orgulho e Preconceito",
+        "autor": "Jane Austen"
+    },
+    {
+        "id": 2,
+        "nome": "O retrato de Dorian Gray",
+        "autor": "Oscar Wild"
+    },
+    {
+        "id": 3,
+        "nome": "Carta ao pai",
+        "autor": "Kafka"
+    }
+];
+
+module.exports = livros;
